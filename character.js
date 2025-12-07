2606f80909696f23aa9fcba8e716587180b382e0
shaye <sm0548540902@gmail.com>
2025-12-07 17:40:12 +0200

    print board

--- character.js
+++ character.js
@@ -1,9 +1,31 @@
-function create_soldjer(type,rank){
+function create_soldjer(type,rank,player){
     return{
+     player:player,
      rank:rank,
      type:type,
      location:{x:undefined,y:undefined}
     }
 }
 
-console.log(create_soldjer("S",5));
+function printBoard(board){
+    let str = ""
+   for (let r of board){
+    str+="["
+    for (let c of r){
+       if (typeof c != 'object'){
+        str+="_"
+       }else{
+        if (c.player === "player"){
+            str+="O"
+        }else{
+            str+="X"
+        }
+       }
+       str+=" "
+    }
+    str+="]\n"
+   }
+   return str
+}
+
+
