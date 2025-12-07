0fa9a0935f0e604c017e981e32061fece7779c3d
shaye <sm0548540902@gmail.com>
2025-12-07 18:08:00 +0200

    print board

---
+++ board.js
@@ -0,0 +1,22 @@
+function printBoard(board){
+    let str = ""
+   for (let r of board){
+    str+="["
+    for (let c of r){
+       if (typeof c != 'object'){
+        str+="_"
+       }else{
+        if (c.player === "player"){
+            str+="O"
+        }else{
+            str+="X"
+        }
+       }
+       str+=" "
+    }
+    str = str.slice(0,str.length-1)
+    str+="]\n"
+   }
+   return str
+}
+console.log(printBoard([["",{},""]]));
