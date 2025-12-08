407712ea8567bf5ca9abcaa731bd4fb1aa9bc9a1
shaye <sm0548540902@gmail.com>
2025-12-08 19:25:00 +0200

    create soldier 2

---
+++ faza2/soldiers.js
@@ -0,0 +1,48 @@
+import createSoldier from "../character.js";
+import readLineSync from "readline-sync"
+
+function createSoldiersplayer2(board) {
+    const ranks = [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10]
+    const soldiers = ranks.map((rank) => {
+        return createSoldier("S", rank, "player")
+    })
+    const flag = createSoldier("F", 0, "player")
+    soldiers.push(flag)
+    soldiers.forEach((s)=>{
+        while (true){
+          console.log(`type ${s.type} rank ${s.rank}`)
+          let row = readLineSync.question("wich row 0-3: ")
+          let col = readLineSync.question("wich col: ")
+          if (row != 0 && row != 1 && row != 2 && row != 3){
+            continue
+          }
+          board[row][col] = s
+          s.location.x = row
+          s.location.y = col
+          break
+        }
+    })
+}
+
+function createSoldiersAI2(board){
+    const ranks = [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10]
+    const soldiers = ranks.map((rank) => {
+        return createSoldier("S", rank, "AI")
+    })
+    const flag = createSoldier("F", 0, "AI")
+    soldiers.push(flag)
+    soldiers.forEach((s)=>{
+        while (true){
+            let row = Math.floor(Math.random()*4)+6
+            let col = Math.floor(Math.random()*board[0].length)
+            if (typeof board[row][col] === 'object'){
+                continue
+            }
+            board[row][col] = s
+            s.location.x = row
+            s.location.y = col
+            break
+        }
+    })
+}
+
