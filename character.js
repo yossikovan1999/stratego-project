5dc63b6fef0842459e442911376e21018c767a1d
Yossi Kovan <yossikovan@gmail.com>
2025-12-08 09:52:33 +0200

    fixing the compare characters logic

--- character.js
+++ character.js
@@ -12,29 +12,5 @@ function createSoldier(type, rank, player) {
 
 
 
-//==========================================
-//           compare characters
-//==========================================
-function compareCharacters(attacker, attacked){
-   
-   //if the attacker is a flag throw a error.
-   if(attacker.type === 'F'){
-    throw new Error("Attacker cannot be a flag, internal game error!");
-   }
-
-  if(attacked.type === 'F'){
-    return "GAME_OVER";
-  }else if(attacker.rank === 1 && attacked.rank === 9){
-    return "ATTACKER_WON";
-  }else if(attacker.rank < attacked.rank){
-    return "ATTACKER_WON";
-  }else if(attacker.rank > attacked.rank){
-    return "ATTACKED_WON";
-  }else{
-    return "TIE";
-  }
-}
-
-
 let t = readLineSync.question();
 console.log(t);
