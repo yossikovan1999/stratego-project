589907acc2b9b131d16740f94722c057cf05837b
Yossi Kovan <yossikovan@gmail.com>
2025-12-08 12:48:06 +0200

    adding the fill board funcions

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import board from "./board/board.js";
-import {printBoard} from "./board/boardFunctions.js";
+import {printBoard, createSoldiers} from "./board/boardFunctions.js";
 import { fillBoard } from "./board/initBoard.js";
 import createSoldier  from "./character.js";
 
 
-function main(){
-    console.log(printBoard(board));
-}
-let s = createSoldier("S",3,"ll",1,1)
-fillBoard([s])
-main();
+
+createSoldiers(0);
+console.log(printBoard(board));
