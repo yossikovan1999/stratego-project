17c8a401b207c24334e2c1414db537ef5565f96c
Yossi Kovan <yossikovan@gmail.com>
2025-12-08 09:44:02 +0200

    resolving conflict

--- character.js
+++ character.js
@@ -9,66 +9,32 @@ function createSoldier(type, rank, player) {
   };
 }
 
-function seeWolking(board, soldjer) {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i]; j++) {
-      if (board[i][j] === soldjer) {
-        if (j > 0) {
-          if (typeof board[i][j - 1] === 'object') {
-            console.log(`in the left have soldjer of ${board[i][j - 1].player}`)
-          } else {
-            console.log("in the left is avlable");
-          }
-        }if (j < board[i].length-1){
-          if (typeof board[i][j + 1] === 'object') {
-            console.log(`in the rigth have soldjer of ${board[i][j + 1].player}`)
-          } else {
-            console.log("in the rigth is avlable");
-          }
-        }if (i > 0){
-          if (typeof board[i-1][j] === 'object') {
-            console.log(`in the up have soldjer of ${board[i-1][j].player}`)
-          } else {
-            console.log("in the up is avlable")
-          }
-        }if (typeof board[i+1][j] === 'object') {
-            console.log(`in the dwon have soldjer of ${board[i+1][j].player}`)
-          } else {
-            console.log("in the down is avlable");
+function seeWolking(board, soldjer) {}
 
-          }
-      }
-    }
-  }
-
-}
-
-
-<<<<<<< HEAD
 
 //==========================================
 //           compare characters
 //==========================================
 function compareCharacters(attacker, attacked){
-  
+   
+   //if the attacker is a flag throw a error.
    if(attacker.type === 'F'){
     throw new Error("Attacker cannot be a flag, internal game error!");
    }
 
-
   if(attacked.type === 'F'){
     return "GAME_OVER";
+  }else if(attacker.rank === 1 && attacked.rank === 9){
+    return "ATTACKER_WON";
   }else if(attacker.rank < attacked.rank){
     return "ATTACKER_WON";
   }else if(attacker.rank > attacked.rank){
     return "ATTACKED_WON";
+  }else{
+    return "TIE";
   }
-
-
 }
 
 
 let t = readLineSync.question();
 console.log(t);
-=======
->>>>>>> 918cae514f303b148c21b5dc5fd24104460172e9
