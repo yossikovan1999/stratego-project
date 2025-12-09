1a36f4d222b4e2f26097e400114f838ff8779096
shaye <sm0548540902@gmail.com>
2025-12-09 11:54:48 +0200

    add readme

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 import board from "./board/board.js";
-import {printBoard, createSoldiers} from "./board/boardFunctions.js";
+import { printBoard, createSoldiers } from "./board/boardFunctions.js";
 import { fillBoard } from "./board/initBoard.js";
-import createSoldier  from "./character.js";
-import {seeWolking,moveSoldier} from "./game/moving.js"
-function game(){
-    const soldier = createSoldier("S",Math.floor(Math.random()*9)+1,"player",9,Math.floor(Math.random()*10))
-    createSoldiers(0,"AI")
+import createSoldier from "./character.js";
+import { seeWolking, moveSoldier } from "./game/moving.js"
+function game() {
+    const soldier = createSoldier("S", Math.floor(Math.random() * 9) + 1, "player", 9, Math.floor(Math.random() * 10))
+    createSoldiers(0, "AI")
     fillBoard([soldier])
-    console.log( printBoard(board));
-    while (true){
-    seeWolking(board,soldier)
-    const result = moveSoldier(soldier)
-    console.log(printBoard(board));
-     if (result === "GAME_OVER"){
-        console.log("you won by by");
-        break
-     }
-    if (result === "ATTACKED_WON" || result === "TIE"){
-        console.log("you lost by by");
-        break
+    printBoard(board)
+    while (true) {
+        seeWolking(board, soldier)
+        const result = moveSoldier(soldier)
+        printBoard(board)
+        if (result === "GAME_OVER") {
+            console.log("you won by by");
+            break
+        }
+        if (result === "ATTACKED_WON" || result === "TIE") {
+            console.log("you lost by by");
+            break
+        }
+        for (let i = 0; i < board.length; i++) {
+        }
     }
-    for (let i = 0;i<board.length;i++){
-}
-}
 }
 
 
